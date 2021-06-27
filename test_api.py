a1a7ec6c6c2a8da99103828752a5fdf42f73cea3
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-06-26 23:50:50 -0700

    api test

--- test_api.py
+++ test_api.py
@@ -1,5 +1,9 @@
 import flask_app
 import requests
 
-input_folder = '../input'
+input_folder = '../input/'
+local_host_url = 'http://127.0.0.1:5000/'
 
+def test_file_upload():
+    files = {'file': open(input_folder + 'A.xml', 'rb')}
+    r = requests.post(local_host_url, files = files)
