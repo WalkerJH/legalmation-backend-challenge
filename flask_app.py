ab3bcd7011d42ccb379f4579d12e1c67cbc771f7
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-06-27 19:36:03 -0700

    initial html

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+TEMPLATE_DIR = os.path.abspath('templates')
+STATIC_DIR = os.path.abspath('static')
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
 @app.route("/")
 def hello():
-    return "Hello world"
+    return render_template('mainpage.html')
 
 if __name__ == "__main__":
     app.run()
