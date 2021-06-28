f86ebf63eaaa33367a2f8c55232dd57309bf1056
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-06-27 22:33:15 -0700

    Documentation on web portal; styling

--- test_api.py
+++ test_api.py
@@ -6,4 +6,9 @@ local_host_url = 'http://127.0.0.1:5000/'
 
 def test_file_upload():
     files = {'file': open(input_folder + 'A.xml', 'rb')}
+    requests.post(local_host_url, files = files)
+
+def test_api_response():
+    files = {'file': open(input_folder + 'A.xml', 'rb')}
     r = requests.post(local_host_url, files = files)
+    assert(r.json) != None
