114141de2226a5208e087a00424e6fed6969613b
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-06-27 23:37:59 -0700

    file upload button

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import os
+import complaint_parser
+import sqlite3
 
 TEMPLATE_DIR = os.path.abspath('templates')
 STATIC_DIR = os.path.abspath('static')
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
-@app.route("/")
-def hello():
+@app.route('/', methods = ['GET', 'POST'])
+def mainpage():
     return render_template('mainpage.html')
 
-if __name__ == "__main__":
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return process_xml(f.read())
+
+def process_xml(xml_text):
+    return xml_text
+
+if __name__ == '__main__':
     app.run()
